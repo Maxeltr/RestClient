4b455b18b5de2cdddd7f0df7acceef98541bf5de
max <maksim.eltratov@ya.ru>
2019-09-30 22:55:01 +0800

    добавил tableview для вывода списка файлов (добавить возможность выбора где искать файлы на сервере или локально)

--- src/main/java/ru/maxeltr/rstclnt/FXMLController.java
+++ src/main/java/ru/maxeltr/rstclnt/FXMLController.java
@@ -5,15 +5,17 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 import javafx.stage.DirectoryChooser;
 import java.io.File;
+import java.text.SimpleDateFormat;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Parent;
+import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
 import javafx.stage.Window;
-import javafx.scene.control.ListView;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class FXMLController implements Initializable {
 
@@ -21,12 +23,36 @@ public class FXMLController implements Initializable {
     private Parent root;
 
     @FXML
-    private ListView fileList;
+    private TableView<FileModel> fileTable;
 
     @FXML
-    private Label label;
+    private TableColumn<FileModel, Integer> name;
 
-    private ObservableList<String> items = FXCollections.observableArrayList();
+    @FXML
+    private TableColumn<FileModel, String> date;
+
+    @FXML
+    private TableColumn<FileModel, String> type;
+
+    private ObservableList<FileModel> items = FXCollections.observableArrayList();
+
+    @FXML
+    private void handleChooseFolder(ActionEvent event) {
+        final DirectoryChooser chooser = new DirectoryChooser();
+        Window stage = (Stage) root.getScene().getWindow();
+        File folder = chooser.showDialog(stage);
+        if (folder != null) {
+            File[] files  = folder.listFiles();
+            for (File file : files) {
+                SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+                items.add(new FileModel(file.getName(), sdf.format(file.lastModified()), "" + file.length()));
+            }
+
+            fileTable.setItems(items);
+        } else {
+
+        }
+    }
 
     @FXML
     private void handleButtonAction(ActionEvent event) {
@@ -34,16 +60,17 @@ public class FXMLController implements Initializable {
         Window stage = (Stage) root.getScene().getWindow();
         File folder = chooser.showDialog(stage);
         if (folder != null) {
-            items.add("First task");
-            items.add("Second task");
+
         } else {
-            label.setText(null);
+
         }
 
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        fileList.setItems(items);
+        name.setCellValueFactory(new PropertyValueFactory<>("Name"));
+        date.setCellValueFactory(new PropertyValueFactory<>("Date"));
+        type.setCellValueFactory(new PropertyValueFactory<>("Type"));
     }
 }
